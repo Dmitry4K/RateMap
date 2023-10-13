39051f628ea87bd1040308cc9f37dc9628cd79dc
Dmitry <dmitry.k48@yandex.ru>
2023-10-13 21:29:59 +0300

    gw: update migrations

--- migrations/main.py
+++ migrations/main.py
@@ -1,22 +1,30 @@
 import os
+from typing import Mapping, Any
 
 import pymongo
-from numpy import pi, cos, sin, arccos, arange, arcsin, sqrt
+from numpy import pi, cos, sin, arccos, arange, arcsin, sqrt, sign, column_stack
 from pymongo import MongoClient
 
-# import matplotlib.pyplot as pp
+import matplotlib.pyplot as pp
 
-MONGO_URL = os.environ["MONGO_URL"]
-MONGO_USER = os.environ["MONGO_USER"]
-MONGO_PASS = os.environ["MONGO_PASS"]
-BATCH_SIZE = int(os.environ["BATCH_SIZE"])
-POINTS_COUNT = int(os.environ["POINTS_COUNT"])
-
+MONGO_URL = "mongodb://admin:admin@localhost:27017/"  # os.environ["MONGO_URL"]
+MONGO_USER = ""  # os.environ["MONGO_USER"]
+MONGO_PASS = ""  # os.environ["MONGO_PASS"]
+BATCH_SIZE = 10_000  # int(os.environ["BATCH_SIZE"])
+POINTS_COUNT = 70_000_000  # int(os.environ["POINTS_COUNT"])
+# count and distance (meters)
+# 100_000      50_000 (50km) 1 or 2 nearest points
+# 70_000_000
+# 10000 level of regions
+# 100_000_000 3000  level of towns
+# 1000  level of districts
+# 250   level of city blocks
+# 50    level of buildings
 
 def main():
     client = connect()
     migrate(client)
-    # points(client)
+    points(client)
     close(client)
 
 def connect():
@@ -56,8 +64,10 @@ def migrate(client):
 def points(client):
     batch_count = round(POINTS_COUNT / BATCH_SIZE)
     for batch_number in range(batch_count):
+        print(f"\rGenerating batch {batch_number+1}/{batch_count} with size: {BATCH_SIZE}", end='')
         pts = batch(batch_number, BATCH_SIZE, POINTS_COUNT)
         load(client, pts)
+
 
 def close(client):
     client.close()
@@ -68,20 +78,39 @@ def batch(batch_number: int, batch_size: int, num_pts: int):
     phi = arccos(1 - 2 * indices / num_pts)
     theta = pi * (1 + 5 ** 0.5) * indices % (2 * pi)
     x, y, z = cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)
-    lat, lng = arcsin(z / sqrt(x * x + y * y + z * z)), arccos(x / sqrt(x * x + y * y))
-    return lat, lng
+    lat, lng = arcsin(z / sqrt(x * x + y * y + z * z)) * 180 / pi, sign(y) * arccos(x / sqrt(x * x + y * y)) * 180 / pi
+    # x, y, z = cos(lat * pi / 180) * cos(lng* pi / 180), cos(lat* pi / 180) * sin(lng* pi / 180), sin(lat* pi / 180)
     # print(max(lat))
     # print(min(lat))
-    # print(max(lon))
-    # print(min(lon))
+    # print(max(lng))
+    # print(min(lng))
     # pp.figure(figsize=(16, 12)).add_subplot(111, projection='3d').scatter(x, y, z)
     # pp.xlabel('X')
     # pp.ylabel('Y')
     # pp.show()
+    return lat, lng
 
 
-def load(client, points):
-    pass
+def load(client: MongoClient[Mapping[str, Any]], pts):
+    lat = pts[0]
+    lng = pts[1]
+    stacked = column_stack((lat, lng))
+    objects = [
+        {
+            "mark": 0.0,
+            "count": 0.0,
+            "point": {
+                "type": "Point",
+                "coordinates": [
+                    x[1],
+                    x[0]
+                ]
+            },
+            "_class": "ru.dmitry4k.geomarkback.data.dao.GeoPointDao"
+        } for x in stacked
+    ]
+    resp = client["local"]["points"].insert_many(objects)
+    # print(f"Successfully inserted {len(resp.inserted_ids)} rows")
 
 
 if __name__ == '__main__':
