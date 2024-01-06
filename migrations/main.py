114428ef9843c910d2f59eb1cd7164612cd31a53
Dmitry <dmitry.k48@yandex.ru>
2024-01-07 02:29:59 +0300

    mg: support config

--- migrations/main.py
+++ migrations/main.py
@@ -1,13 +1,17 @@
 import os
 import time
+import yaml
+import numpy as np
+import geopy.distance as geodist
+import random
 from math import ceil
 from typing import Mapping, Any
 
 import pymongo
-from numpy import pi, cos, sin, arccos, arange, arcsin, sqrt, sign, column_stack
+from numpy import pi, cos, sin, arccos, arange, arctan2
 from pymongo import MongoClient
 
-import matplotlib.pyplot as pp
+# import matplotlib.pyplot as pp
 
 MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASS = os.getenv("MONGO_PASS")
@@ -15,8 +19,10 @@ MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 MONGO_CRT_PATH = os.getenv("MONGO_CRT_PATH")
 MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_DB_URL}/?authMechanism=DEFAULT&tls=true&tlsCAFile={MONGO_CRT_PATH}"
 BATCH_SIZE = 10_000
-POINTS_COUNT = 100_000_000 #250_000_000
 DATABASE_NAME = "ratemap-mongo-db"
+RATEMAP_CONFIG_PATH = os.getenv("RATEMAP_CONFIG_PATH")
+
+
 # count and distance (meters)
 # 100_000      50_000 (50km) 1 or 2 nearest points
 # 10_000_000 8000-3 nearest
@@ -41,29 +47,47 @@ DATABASE_NAME = "ratemap-mongo-db"
 
 # !!!! Возможное улучшение - расчитать точки ТОЛЬКО для России, т.к. ее территория занимает 13% поверхности Земли.
 def main():
-    # d = 50
-    # R = 6378
-    # print(16*R*R*1000*1000/d/d)
-    # c = 250_000_000
-    # print(sqrt(16*R*R*1000*1000/c))
+    config = read_config(RATEMAP_CONFIG_PATH)
     client = connect()
-    migrate(client)
-    points(client)
+    create_points(client, config)
     close(client)
+
+
+def read_config(path: str):
+    with open(path, "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
 
 def connect():
     client = MongoClient(MONGO_URL)
     return client
 
 
-def migrate(client):
-    tablename = "points"
+def create_points(client, config):
+    layers = config['layers']
+    areas = config['areas']
+    for layer in layers:
+        for area in areas:
+            table_name = area + '_' + layer
+            pts_count = 16 * 6400 * 6400 * 1000 * 1000 / layers[layer]['searchDistance']
+            batch_size = BATCH_SIZE
+            lat = areas[area]['center']['lat']
+            lng = areas[area]['center']['lng']
+            radius = areas[area]['radius']
+            create_table(client, table_name)
+            points(client, table_name, pts_count, batch_size, lat, lng, radius)
+
+
+def create_table(client, table_name):
     db = client[DATABASE_NAME]
     colla = {'locale': 'en_US', 'strength': 2, 'numericOrdering': True, 'backwards': False}
-    if tablename not in db.list_collection_names():
-        print(f"Creating collection {tablename} ...")
+    if table_name not in db.list_collection_names():
+        print(f"Creating collection {table_name} ...")
         db.create_collection(
-            name=tablename,
+            name=table_name,
             codec_options=None,
             read_preference=None,
             write_concern=None,
@@ -72,9 +96,9 @@ def migrate(client):
             collation=colla
         )
     else:
-        print(f"Collection {tablename} is already exists.")
+        print(f"Collection {table_name} is already exists.")
 
-    collection = db[tablename]
+    collection = db[table_name]
     types = [x['key'][0][1] for x in collection.index_information().values()]
     if '2dsphere' not in types:
         print("Creating geospatial index ...")
@@ -85,28 +109,50 @@ def migrate(client):
     return client
 
 
-def points(client):
-    batch_count = ceil(POINTS_COUNT / BATCH_SIZE)
+def points(client, table_name, pts_count, batch_size, lat, lng, radius):
+    batch_count = ceil(pts_count / batch_size)
     start = time.time()
+    elapsed_points = radius * radius * pts_count / (4 * 6400 * 6400 * 1000 * 1000)
     for batch_number in range(batch_count):
-        print(f"\rGenerating batch {batch_number+1}/{batch_count} with size: {BATCH_SIZE}", end='')
-        pts = batch(batch_number, BATCH_SIZE, POINTS_COUNT)
-        load(client, pts)
+        curr_points_num = batch_number * batch_size
+        print(
+            f"\rGenerated {curr_points_num}({curr_points_num * 100 / elapsed_points:.2f}%) "
+            f"points for table: {table_name}, elapsed time: {time.time() - start:.2f}",
+            end='')
+        pts = batch(batch_number, batch_size, pts_count, lat, lng)
+        pts = np.dstack((pts[1], pts[0])).reshape((batch_size, 2))
+        checking = check_batch(pts, radius, lat, lng, 25)
+        if len(pts) > 0:
+            load(client, pts, table_name)
+        if not checking:
+            print()
+            return
     print()
-    print(f"All points was generated in {time.time() - start}")
 
 
 def close(client):
     client.close()
 
 
-def batch(batch_number: int, batch_size: int, num_pts: int):
+def check_batch(pts, radius, lat, lng, pts_check_count) -> bool:
+    for _ in range(pts_check_count):
+        p = pts[random.randint(0, len(pts)-1)]
+        if radius < geodist.geodesic((p[1], p[0]), (lat, lng)).m:
+            return False
+    return True
+
+
+def batch(batch_number: int, batch_size: int, num_pts: int, lat_center, lng_center):
     indices = arange(batch_number * batch_size, (batch_number + 1) * batch_size, dtype=float) + 0.5
     phi = arccos(1 - 2 * indices / num_pts)
     theta = pi * (1 + 5 ** 0.5) * indices % (2 * pi)
+    la = lat_center * pi / 180
+    ln = -(lng_center * pi / 180 + pi / 2)
     x, y, z = cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)
-    lat, lng = arcsin(z / sqrt(x * x + y * y + z * z)) * 180 / pi, sign(y) * arccos(x / sqrt(x * x + y * y)) * 180 / pi
-    # x, y, z = cos(lat * pi / 180) * cos(lng* pi / 180), cos(lat* pi / 180) * sin(lng* pi / 180), sin(lat* pi / 180)
+    x1, y1, z1 = x, cos(la) * y + sin(la) * z, -sin(la) * y + cos(la) * z
+    x2, y2, z2 = cos(ln) * x1 - sin(ln) * z1, y1, sin(ln) * x1 + cos(ln) * z1
+    lat = (pi / 2 - arccos(y2)) * (180 / pi)
+    lng = (pi - arctan2(z2, x2)) * (180 / pi) - 180
     # print(max(lat))
     # print(min(lat))
     # print(max(lng))
@@ -122,25 +168,19 @@ def batch(batch_number: int, batch_size: int, num_pts: int):
     return lat, lng
 
 
-def load(client: MongoClient[Mapping[str, Any]], pts):
-    lat = pts[0]
-    lng = pts[1]
-    stacked = column_stack((lat, lng))
+def load(client: MongoClient[Mapping[str, Any]], pts, table_name):
     objects = [
         {
             "mark": 0.0,
             "count": 0.0,
             "point": {
                 "type": "Point",
-                "coordinates": [
-                    x[1],
-                    x[0]
-                ]
+                "coordinates": [x[0], x[1]]
             },
             "_class": "ru.dmitry4k.geomarkback.data.dao.GeoPointDao"
-        } for x in stacked
+        } for x in pts
     ]
-    resp = client[DATABASE_NAME]["points"].insert_many(objects)
+    resp = client[DATABASE_NAME][table_name].insert_many(objects)
     # print(f"Successfully inserted {len(resp.inserted_ids)} rows")
 
 
