18cb62d69a3a7d77df184ac7e959a5c97c21b71e
Dmitry <dmitry.k48@yandex.ru>
2024-01-06 20:57:03 +0300

    aa: change url

--- android/app/src/main/java/com/example/ratemapapp/map/provider/RateMapUrlProviderImpl.kt
+++ android/app/src/main/java/com/example/ratemapapp/map/provider/RateMapUrlProviderImpl.kt
@@ -6,6 +6,6 @@ import com.yandex.mapkit.tiles.UrlProvider
 
 class RateMapUrlProviderImpl : UrlProvider {
     override fun formatUrl(tileId: TileId, version: Version): String {
-        return "http://10.0.2.2:8080/tile/yandex/png?x=${tileId.x}&y=${tileId.y}&z=${tileId.z}"
+        return "https://ratemap.ru/api/tile/yandex/png?x=${tileId.x}&y=${tileId.y}&z=${tileId.z}"
     }
 }
