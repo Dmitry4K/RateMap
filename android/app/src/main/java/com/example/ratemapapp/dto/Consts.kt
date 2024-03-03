0d2069bc64229bf50129d8bfdd09397938957e5e
Dmitry <dmitry.k48@yandex.ru>
2024-03-03 22:52:55 +0300

    aa: update localhost endpoint

--- android/app/src/main/java/com/example/ratemapapp/dto/Consts.kt
+++ android/app/src/main/java/com/example/ratemapapp/dto/Consts.kt
@@ -2,5 +2,5 @@ package com.example.ratemapapp.dto
 
 object Consts {
     const val RATEMAP_API_URL = "https://ratemap.ru/api"
-    //const val RATEMAP_API_URL = "http://10.0.2.2:8080/api/"
+    //const val RATEMAP_API_URL = "http://10.0.2.2:8080/api"
 }
