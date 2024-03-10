cb765ea7271f0c7ce6039b9d8b82aa386c127d5c
Dmitry <dmitry.k48@yandex.ru>
2024-03-10 19:45:51 +0300

    aa: update urls

--- android/app/src/main/java/com/example/ratemapapp/map/provider/RateMapUrlProviderImpl.kt
+++ android/app/src/main/java/com/example/ratemapapp/map/provider/RateMapUrlProviderImpl.kt
@@ -13,6 +13,6 @@ class RateMapUrlProviderImpl : UrlProvider {
         val projection = Projections.getWgs84Mercator()
         val point = projection.xyToWorld(XYPoint(tileId.x.toDouble() + 0.5, tileId.y.toDouble() + 0.5), tileId.z)
         Log.i("URL", "lat: ${point.latitude}, lng: ${point.longitude}, x: ${tileId.x}, y: ${tileId.y}, z: ${tileId.z}")
-        return "$RATEMAP_API_URL/tile/yandex/png?x=${tileId.x}&y=${tileId.y}&z=${tileId.z}"
+        return "$RATEMAP_API_URL/v1/layer/marks/tile/yandex/png?x=${tileId.x}&y=${tileId.y}&z=${tileId.z}"
     }
 }
