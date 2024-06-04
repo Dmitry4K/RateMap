c49ed669a24a891ce6ba67aaeac1b01edfd2defb
Dmitry <dmitry.k48@yandex.ru>
2024-06-05 01:49:16 +0300

    aa: fix project

--- android2/app/src/main/java/com/example/ratemapapp/activity/MapKitActivity.kt
+++ android2/app/src/main/java/com/example/ratemapapp/activity/MapKitActivity.kt
@@ -12,13 +12,21 @@ private const val API_KEY = "60e03fd4-fbfe-4b18-9a54-42407c8b609d"
 
 abstract class MapKitActivity: AppCompatActivity() {
     protected lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val dexOutputDir: File = codeCacheDir
         dexOutputDir.setReadOnly()
         super.onCreate(savedInstanceState)
         Log.i("App", "OnCreate executed")
-        MapKitFactory.setApiKey(API_KEY)
+        if (savedInstanceState?.getBoolean("init") != true) {
+            MapKitFactory.setApiKey(API_KEY)
+        }
         MapKitFactory.initialize(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("init", true)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
